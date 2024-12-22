cd1bb169fe6d61573d252ceff69ffccca94ea77f
haivoDA22TTD <vochihai7@gmail.com>
2024-12-22 10:00:17 +0700

    Delete src directory

--- src/project/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if( !isset($_SESSION['tentaikhoan'])){
-        header('location:login.php');
-        exit();
-    }
-    echo 'xinchao' . $_SESSION['tentaikhoan'] . '<br>';
-    echo '<a href="logout.php">Dang Xuat</a>';
-?>
