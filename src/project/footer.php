670abf3ab4dcc22aad2d483ce946ce655167e7be
haivoDA22TTD <vochihai7@gmail.com>
2025-01-08 22:17:47 +0700

    Delete src/project directory

--- src/project/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="mt-5 p-4 bg-dark text-white text-center">
-  <p>Footer</p>
-</div>
-</body>
-</html>
--- src/project/nguoiquantri.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once("header.php");
-?>
-<div class = "container mt-5">
-Em cần viết bổ sung thêm đoạn hướng dẫn cách sử dụng các tính năng có trong hệ thống
-</div>
-<?php
-    include_once("footer.php");
-?>
